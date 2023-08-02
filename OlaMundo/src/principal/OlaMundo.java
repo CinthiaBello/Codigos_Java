64e58c67b8a5e613f6f025764b48e65732e3c43f
Luan Rocha <luan.silva.r@hotmail.com>
2023-08-02 17:26:28 -0300

    codigos iniciais de progarmaçao java

---
+++ OlaMundo/src/principal/OlaMundo.java
@@ -0,0 +1,19 @@
+package principal;
+//classe principal
+public class OlaMundo {
+    //metodo principal, onde executa o programa
+	public static void main(String[] args) {
+		
+		//imprime uma linha e ao final salta uma nova liha
+		//print = imprimir;ln = new line;
+		System.out.println("Hello world!");
+
+		// comentario de uma linha
+		/*
+		 * comentário de 
+		 * multiplas 
+		 * linhas
+		 */
+	}
+
+}
