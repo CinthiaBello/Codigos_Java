74cfb3027a37aee0d7f50b82ba5dd4e7e1f61721
Luan Rocha <luan.silva.r@hotmail.com>
2023-08-04 15:00:45 -0300

    Média das notas bimestrais

---
+++ MédiaBimestral/src/principal/Media.java
@@ -0,0 +1,18 @@
+package principal;
+
+public class Media {
+
+	public static void main(String[] args) {
+		
+		double nota1 = 8.5;
+		double nota2 = 10;
+		double nota3 = 5.5;
+		double nota4 = 9.4;
+		
+		double media = (nota1 + nota2 + nota3 + nota4)/4;
+		
+		System.out.println("Média bimestral: " + media);
+
+	}
+
+}
