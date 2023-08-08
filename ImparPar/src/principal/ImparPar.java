398be49abfa81bc75defdc45132e09dee734fda3
Luan Rocha <luan.silva.r@hotmail.com>
2023-08-08 14:30:48 -0300

    Atualização do programa impar/par

--- ImparPar/src/principal/ImparPar.java
+++ ImparPar/src/principal/ImparPar.java
@@ -1,21 +1,22 @@
 package principal;
 
-
+import java.util.Scanner;
 
 public class ImparPar {
 
 	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in); 
 		
+		int numero = 0;
 		
-		
-		int numero = 5;
-		
+		System.out.println("Digite um número: ");
+		numero = leia.nextInt();
 		if (numero % 2 == 0) {
 			System.out.println("Número par");
 		}else {
 			System.out.println("Número impar");
 		}
-		
+		leia.close();
 		
 
 	}
