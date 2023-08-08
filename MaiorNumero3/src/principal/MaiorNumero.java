6fe113c116f943e2e06201a75828305c64e6d7de
Luan Rocha <luan.silva.r@hotmail.com>
2023-08-08 14:53:25 -0300

    Atualização do programa maior número entre 3

--- MaiorNumero3/src/principal/MaiorNumero.java
+++ MaiorNumero3/src/principal/MaiorNumero.java
@@ -1,24 +1,34 @@
 package principal;
 
+import java.util.Scanner;
+
 public class MaiorNumero {
 
 	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
 		
-		int numero1 = 5;
-		int numero2 = 10;
-		int numero3 = 123;
+		int numero1 = 0;
+		int numero2 = 0;
+		int numero3 = 0;
+		
+		System.out.println("Digite o primeiro número: ");
+		numero1 = leia.nextInt();
+		System.out.println("Digite o segundo número: ");
+		numero2 = leia.nextInt();
+		System.out.println("Digite o terceiro número: ");
+		numero3 = leia.nextInt();
 		
 		if(numero1 > numero2 && numero1 > numero3){
 			System.out.println("O maior é: " + numero1);
         }
-		if(numero2 > numero3 && numero2 > numero1){
-        	System.out.println("O maior é: " + numero2);
+		else if(numero2 > numero3 && numero2 > numero1){
+			System.out.println("O maior é: " + numero2);
         }
-        if(numero3 > numero1 && numero3 > numero2){
+		else {
         	System.out.println("O maior é: " + numero3);
         }
        
-			
+		leia.close();	
 
 	}
 
