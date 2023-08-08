ccde9acc934b3b95141371caaf428115bd881660
Luan Rocha <luan.silva.r@hotmail.com>
2023-08-08 15:29:40 -0300

    Atualização do programa calcula a media

--- Media/src/principal/Media.java
+++ Media/src/principal/Media.java
@@ -1,25 +1,41 @@
 package principal;
 
+import java.util.Scanner;
+
 public class Media {
 
 	public static void main(String[] args) {
-		double nota1 = 4.5;
-		double nota2 = 10;
-		double nota3 = 6.7;
-		double nota4 = 3.3;
+		Scanner leia = new Scanner(System.in);
+		
+		double nota1 = 0;
+		double nota2 = 0;
+		double nota3 = 0;
+		double nota4 = 0;
 		double media = 0;
+		
+		System.out.println("Digite a primeira nota");
+		nota1 = leia.nextDouble();
+		System.out.println("Digite a segunda nota");
+		nota2 = leia.nextDouble();
+		System.out.println("Digite a terceira nota");
+		nota3 = leia.nextDouble();
+		System.out.println("Digite a quarta nota");
+		nota4 = leia.nextDouble();
 		
 		media = (nota1 + nota2 + nota3 + nota4)/4;
 		
 		System.out.println("Média: " + media);
 		
 		if (media < 6) {
-			System.out.println("Reprovado");
+			System.out.println("Reprovado" + media);
 		}
-		if (media == 6 && media == 7) {
-			System.out.println("Recuperação");
+		else if (media >= 6 && media < 7) {
+			System.out.println("Recuperação" + media);
+		}else if(media >= 7 && media < 10){
+			System.out.println("Aprovado" + media);
 		}else {
-			System.out.println("Aprovado");
+			System.out.println("Aprovado com honra" + media);
+			
 		}
 		
 	}
