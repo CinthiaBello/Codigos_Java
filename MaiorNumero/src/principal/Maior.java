a41ff278ee1a7d64328d36aec35334d5bcd7fd64
Luan Rocha <luan.silva.r@hotmail.com>
2023-08-08 14:39:01 -0300

    Atualização do programa maior número entre 2

--- MaiorNumero/src/principal/Maior.java
+++ MaiorNumero/src/principal/Maior.java
@@ -1,13 +1,24 @@
 package principal;
 
+import java.util.Scanner;
+
 public class Maior {
 
 	public static void main(String[] args) {
+		Scanner leia = new Scanner(System.in);
 		
-		 int numero1 = 5;
-		 int numero2 = 9;
+		
+		 int numero1 = 0;
+		 int numero2 = 0;
+		 
+		 System.out.println("Digite o primeiro número: ");
+		 numero1 = leia.nextInt();
+		 System.out.println("Digite o segundo número: ");
+		 numero2 = leia.nextInt();
 		 
 		 System.out.println(Math.max(numero1, numero2));
+		 
+		 leia.close();
 		 
 
 	}
