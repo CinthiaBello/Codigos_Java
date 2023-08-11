9de2a19c11b9a32563f9fbdf5711d3f274d1d1c7
Luan Rocha <luan.silva.r@hotmail.com>
2023-08-11 17:44:30 -0300

    Exercício do dia 11/08/23

--- ExercíciosJAVA/src/principal/Exercícios.java
+++ ExercíciosJAVA/src/principal/Exercícios.java
@@ -8,30 +8,36 @@ public class Exercícios {
 
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
-		
+		// variaveis que recebe os nomes dos times
 		String time1 = "";
 		String time2 = "";
+		// variaveis que recebe a qntidade de gols
 		int golsTime1 = 0;
 		int golsTime2 = 0;
 		
 		
-		
+		//Pede para o usuário e lê o nome do time 1
 		System.out.println("Digite o nome do primeiro time: ");
 		time1 = leia.next();
+		//Pede para o usuário e lê o nome do time 2
 		System.out.println("Digite o nome do primeiro time: ");
 		time2 = leia.next();
+		//Pede para o usuário e lê a qntdade de gols do time 1
 		System.out.println("Digite o número de gols do primeiro time: ");
 		golsTime1 = leia.nextInt();
+		//Pede para o usuário e lê a qntdade de gols do time 2
 		System.out.println("Digite o número de gols do segundo time: ");
 		golsTime2 = leia.nextInt();
 		
-		
+		// verifica se deu empate
 		if(golsTime1 == golsTime2){
 			System.out.println("Empate!");
 		}
+		//verifica se o time1 é o vencedor
 		if(golsTime1 > golsTime2) { 
 			System.out.println(time1 + " é o vencedor!");
 		}
+		//verifica se o time 2 é o vencedor
 		if(golsTime1 < golsTime2){
 			System.out.println(time2 + " é o vencedor");
 		}
--- ExercíciosJAVA/src/principal/Vetor.java
+++ ExercíciosJAVA/src/principal/Vetor.java
@@ -11,9 +11,28 @@ public class Vetor {
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
 		
+		//um vetor de 20 posicoes
 		int [] numeros = new int[20];
-		for(int contador = 1; contador < 21; contador++) {
-			System.out.println(contador);
+		//Uma variavel para a soma
+		int soma = 0;
+		//um contador para passar em todas as posiçõs 
+		for(int contador = 0; contador < numeros.length; contador++) {
+			System.out.println("Entre com o número: ");
+			//somente números positivos
+			while(numeros[contador] < 0 ) {
+				System.out.println("Número inválido!");
+				System.out.println("Entre com o número: ");
+				numeros[contador] = leia.nextInt()
+;			}
+			//calculo de todos os numeros digitados
+			soma+= numeros[contador];
+		}
+		// imprime a soma dos números e os números digitados
+		System.out.println("A soma dos números " + soma);
+		System.out.println("Números digitados: ");
+		//lista dos numeros digiitados
+		for (int numero : numeros) {
+			System.out.println(numero);
 		}
 		leia.close();
 
